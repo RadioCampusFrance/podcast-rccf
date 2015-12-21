64c1840744bd43a0b4c4b561415c8649a9f51328
Jean-Marie Favreau <J-Marie.Favreau@udamail.fr>
2015-12-21 17:54:22 +0100

    Prise en charge de l'encodage UTF8

--- ws/log_paulo.php
+++ ws/log_paulo.php
@@ -17,7 +17,10 @@ function convert_date($timestamp) {
 }
 
 try {
-	$pdo_options[PDO::ATTR_ERRMODE] = PDO::ERRMODE_EXCEPTION;		
+	$pdo_options[PDO::ATTR_ERRMODE] = PDO::ERRMODE_EXCEPTION;
+        $pdo_options[PDO::MYSQL_ATTR_INIT_COMMAND] = 'SET NAMES utf8';
+	
+	
 
 	$bdd = new PDO('mysql:host='._PAULODB_SERVEUR.';dbname='._PAULODB_BD, _PAULODB_LOGIN, _PAULODB_MDP, $pdo_options);
 
